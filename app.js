ce185ea130071515b1c855ead515802c3e2b43a4
Naoki Teguri <gurin140@gmail.com>
2021-01-30 03:43:33 +0900

    Add authenticated middleware

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ const bodyParser = require('koa-bodyparser')
 
 const errorHandler = require('./errorHandler')
 const authRoute = require('./routes/auth')
+const authenticated = require('./middleware/authenticated') 
+const petsRoute = require('./routes/pets')
 
 const app = new Koa()
 const router = new Router()
@@ -11,6 +13,7 @@ const router = new Router()
 app.use(errorHandler)
 
 router.post('/auth', bodyParser(), authRoute)
+router.get('/my-pets', authenticated, petsRoute)
 
 app
   .use(router.routes())
